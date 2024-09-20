f4656926c75c8bf995d4c9f36c91642a293f11b4
Wajih Wanis <90794139+Wajih-Wanis@users.noreply.github.com>
2024-09-20 19:19:59 +0100

    Time: 56 ms (82.70%), Space: 17.1 MB (41.65%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -0,0 +1,6 @@
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        reversed_s = s[::-1]
+        for i in range(len(s)+1):
+            if s.startswith(reversed_s[i:]):
+                return reversed_s[:i]+s
